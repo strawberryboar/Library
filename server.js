9b86e837bfadcf3ff86be5b83cb2b891413d1e77
Athena Petrovich <51300273+strawberryboar@users.noreply.github.com>
2023-09-29 13:44:10 -0600

    set up mongodb

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+// vvv logs error in console vvv
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
 app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
